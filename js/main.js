ce8e086b44521f1976a7f50ca3b528a8a8ad2193
Jam Timbol <jam.timbol00@gmail.com>
2023-06-29 15:48:15 +0200

    90% done- Result Issue only

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
+
 //projetto insicurissimo cit bryan
 
 /* ULTIMATE PASS GEN-OBV FARA DA SCHIFO
-
 -name
 -surname
 -color
 -fave num1
 -fave num2
-
-
 */
-//start 
 
-//ask basic information
+
+//information
 let Name = prompt("Insert your name:");
 let Surname = prompt("Insert your surname:");
 let Color = prompt("Insert your favorite color:");
 let Number1 = parseInt(prompt("Insert first number:"));
 let Number2 = parseInt(prompt("Insert second number:"));
 
+let sumNumber = Number1 / Number2;
+let password = Name + Surname + Color + sumNumber;
 
-// be sure of the info acquired are correct
-
-
-
-//prepare the template with the info 
-let bigliettoDaVisita = `--biglietto da visita--
-    -Name: ${Name}
-    -Surname: ${Surname}
-    -Color: ${Color}
-    -Number1: ${Number1}
-    -Number2: ${Number2}
-    ---
+console.log(
 `
+-Name: ${Name}
+-Surname: ${Surname}
+-Color: ${Color}
+-Numbers: ${sumNumber}
+`);
 
-let sumNameSurnameColorNumbers = Number1 + Name + Surname + Color + Number2;
-console.log(sumNameSurnameColorNumbers);
-//stamp info to console
 
-//ine
+//html Id
+
+document.getElementById(password).innerHTML = password
